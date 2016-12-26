6bfc7c9975234930c46b4193bd494a90cb0d2ef5
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2016-12-25 22:11:39 -0800

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("\n enter any number:");
+    scanf("%d",&n);
+    if(n>0)
+      printf("\n the number is positive");
+    else
+      printf("\n the number is negative");
+     getch();
+    return 0;
+}
