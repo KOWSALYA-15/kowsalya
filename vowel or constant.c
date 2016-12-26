569245b55e7f2b379533e3599f820715e9a84dc3
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2016-12-25 22:25:34 -0800

    Add files via upload

---
+++ vowel or constant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ char ch;
+ printf("enter any character \n");
+ scanf("%c",&ch);
+ if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    printf("\n is a vowel");
+ else
+    printf("\n is a constant");
+ getch();
+ return 0;
+}
