bf9ed15b3cbf14644f75ee9b99c2869bc0f434a6
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2017-07-03 14:53:39 +0530

    Add files via upload

---
+++ Workingdays.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+public class Workingdays {
+	public static void main(String[] args) {
+	       Scanner sc=new Scanner(System.in);
+	       String str=sc.next();
+	       if(str.equals("sunday")){
+	    	   System.out.println("false");
+	       }
+	       else{
+	    	   System.out.println("True");
+	       }
+	       sc.close();
+		}
+	}
+
+
+
