3929a8c3bdef6f5d6716616e82bb0e3d009fb067
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2017-07-17 12:30:41 +0530

    Add files via upload

---
+++ PrintASCIIValues.java
@@ -0,0 +1,16 @@
+package guvi;
+
+public class PrintASCIIValues {
+
+	public static void main(String[] args) {
+  	  System.out.println("a to z:");
+      for(int i='a';i<='z';i++){
+    	  System.out.println((int)i);
+      }
+	  System.out.println("A to Z:");
+      for(int i='A';i<='Z';i++){
+    	  System.out.println((int)i);
+      }
+	}
+
+}
