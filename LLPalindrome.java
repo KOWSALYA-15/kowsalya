57ef2344773b55c109c2907842d6bcaf702def41
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2017-07-17 14:27:50 +0530

    Add files via upload

---
+++ LLPalindrome.java
@@ -0,0 +1,23 @@
+package guvi;
+import java.util.*;
+public class LLPalindrome {
+
+	public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     LinkedList<Character> l=new LinkedList<Character>();
+     for(int i=0;i<n;i++){
+    	 char ch=sc.next().charAt(0);
+    	 l.add(ch);
+     }
+     LinkedList<Character> l1=new LinkedList<Character>(l);
+     Collections.reverse(l1);
+     if(l1.equals(l)){
+    	 System.out.println("The LinkedList is Palindrome");
+     }
+     else{
+    	 System.out.println("The LinkedList is not Palindrome");
+     }
+     sc.close();
+	}
+}
