71e51b7d545fd5923cbdfcf3dcf552b42ccc499b
KOWSALYA-15 <kalaikowsiMK@gmail.com>
2017-07-19 11:50:35 +0530

    Add files via upload

---
+++ FirstOccurenceOfString.java
@@ -0,0 +1,11 @@
+package guvi;
+public class FirstOccurenceOfString {
+	public static void main(String[] args) {
+      String str[]={"AS","SD","S","AG"};
+      for(int i=0;i<str.length;i++){
+    	  if(str[i].length()==1){
+    		  System.out.println(str[i]);
+    	  }
+      }
+	}
+}
